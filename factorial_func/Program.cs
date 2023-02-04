39a405cd2d2dc15f5ffb9a6f19519e4692dde901
roenbis <bekzat.bagdatuly03@gmail.com>
2023-02-04 19:50:56 +0600

    cs

---
+++ factorial_func/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace factorial_func;
+
+class Program
+{
+    static void Main()
+    {
+        double factorial = 1.0;
+        
+        for (int i = 1; i < 6; ++i)
+        {
+            factorial *= i;
+            Console.WriteLine(factorial);;
+        }
+    }
+}
